43da2b55331cfcd0b364abc82f9d7297b1c5b77d
Park Junhyeon <jeje3792@gmail.com>
2025-06-03 15:44:04 +0900

    feat:CastDTO 구성

---
+++ backend/backend/src/main/java/org/dfpl/lecture/db/backend/dto/CastDTO.java
@@ -0,0 +1,10 @@
+package org.dfpl.lecture.db.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class CastDTO {
+    private String name;
+    private String character;
+    private String profileImageUrl;
+}
