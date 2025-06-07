7227e7176f20a179ffd989d9777c488d548db45e
namisu000 <namisu009@gmail.com>
2025-06-08 02:13:04 +0900

    feat: RatingBar에서 영화 즐겨찾기 추가/제거 기능 구현 및 favoriteApi 연동

--- frontend/src/components/moive/detail/MovieDetailInfo.jsx
+++ frontend/src/components/moive/detail/MovieDetailInfo.jsx
@@ -14,7 +14,7 @@ export const MovieDetailInfo = ({ movie }) => {
           <div className="w-full flex ml-0 sm:ml-8 flex-1 flex-col">
             <section className="w-full mb-2">
               {/* movieId 전달 추가 */}
-              <RatingBar voteAverage={movie.voteAverage} movieId={movie.id} />
+              <RatingBar voteAverage={movie.voteAverage} movieId={movie.id} isFavorite={movie.favorite}/>
             </section>
 
             <section className="w-full mb-10 sm:mb-0 border-t border-t-gray-300">
