1f3512de3c14a8121dd8a87e31bb6e427b6821dc
namisu000 <namisu009@gmail.com>
2025-06-09 02:37:47 +0900

    feat: Not Found 페이지 추가 및 라우트 연결

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,24 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter, useRoutes } from "react-router-dom"
+import {BrowserRouter, useLocation, useRoutes} from "react-router-dom"
 import routes from "~react-pages"
 import App from "./App" // 여기에서 공통 레이아웃, Provider 묶음
 import "./App.css";
+import NotFound from "@/pages/404.jsx";
 
 const Router = () => {
-    const element = useRoutes(routes)
-    return <App>{element}</App>
+    const element = useRoutes([
+        ...routes,
+        {
+            path: "*",
+            element: <NotFound />
+        }
+    ]);
+
+    const location = useLocation();
+    const is404 = location.pathname === "/404" || location.pathname === "*" || !routes.some(route => location.pathname.startsWith(`/${route.path}`));
+
+    return is404 ? element : <App>{element}</App>;
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
