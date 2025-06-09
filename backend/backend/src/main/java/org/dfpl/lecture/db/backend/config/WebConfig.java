d3664ca2428cd23a55c658ad1fc87764fcfe6a47
namisu000 <namisu000@gmail.com>
2025-06-09 23:51:38 +0900

    fix: 로컬 포트가 항상 5173으로 실행되기 위한 세팅

--- backend/backend/src/main/java/org/dfpl/lecture/db/backend/config/WebConfig.java
+++ backend/backend/src/main/java/org/dfpl/lecture/db/backend/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5174") // React dev server
+                .allowedOrigins("http://localhost:5173") // React dev server
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
